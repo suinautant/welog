af369464469c7f64033cc8ddd4d7765cbf2a765b
suinautant <suinautant@gmail.com>
2021-10-04 12:25:22 +0900

    init

---
+++ src/test/java/com/welog/www/WelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welog.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
