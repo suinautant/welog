c958d083e1f810718e11bddfa9683c808be8dc3c
suinautant <suinautant@gmail.com>
2021-10-07 15:49:15 +0900

    init /account/register.html

--- src/main/java/com/welog/www/config/WebSecurityConfig.java
+++ src/main/java/com/welog/www/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 		.authorizeRequests()
 			.antMatchers("/", "/css/**", 
-						"account/register", 
+						"/account/register", 
 						"/article/list", "/article/view").permitAll()
 			.anyRequest().authenticated()
 			.and()
--- src/main/java/com/welog/www/controller/UserController.java
+++ src/main/java/com/welog/www/controller/UserController.java
@@ -2,8 +2,11 @@ package com.welog.www.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.welog.www.model.User;
 import com.welog.www.repository.UserRepository;
 
 @Controller
@@ -13,5 +16,22 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@GetMapping("register")
+	public String register() {
+		return "account/register";
+	}
+	
+	@PostMapping("register")
+	public String registerPost(User user) {
+		user.setEnabled(true);
+		userRepository.save(user);
+		return "redirect:/";
+	}
+	
+	
+	@GetMapping("login")
+	public String login() {
+		return "account/login";
+	}
 
 }
