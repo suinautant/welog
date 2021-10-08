30364e8c2b9815f12c16b244f46b760b1a5caed2
suinautant <suinautant@gmail.com>
2021-10-08 13:59:11 +0900

    add func register

--- src/main/java/com/welog/www/controller/UserController.java
+++ src/main/java/com/welog/www/controller/UserController.java
@@ -2,6 +2,7 @@ package com.welog.www.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class UserController {
 	private UserRepository userRepository;
 	
 	@GetMapping("register")
-	public String register() {
+	public String register(Model model) {
+		
+		model.addAttribute("user", new User());
+
 		return "account/register";
 	}
 	
